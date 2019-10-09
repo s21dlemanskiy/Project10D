870034d02fcc8d5360b24219215677584526506f
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2019-10-09 23:50:00 +0300

    SimpleLogic
    Добавил простую логику и триггер к двери(дверь теперь ведет себя нормально).

---
+++ MagnetGun/Assets/Scripts/Logic/Door_logic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_logic : MonoBehaviour {
+	public Element_Logic logic;
+	public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		anim.SetBool("isOpen", logic.value);
+	}
+}
---
+++ MagnetGun/Assets/Scripts/Logic/Element_Logic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element_Logic : MonoBehaviour {
+	public Element_Logic logic;
+	public bool value;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (logic) {
+			logic.value = value;
+		}
+	}
+}
