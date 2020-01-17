67745b7ac2a9b492d6eef709a34c59f9debf564c
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2020-01-17 22:40:55 +0300

    lvl2 (1\2)
     смена уровней доделана, начат уровень 2

--- MagnetGun/Assets/Scripts/Logic/NewLevel_Logic.cs
+++ MagnetGun/Assets/Scripts/Logic/NewLevel_Logic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NewLevel_Logic : MonoBehaviour {
+	public string level;
 	// Use this for initialization
 	void Start () {
 
@@ -13,7 +14,7 @@ public class NewLevel_Logic : MonoBehaviour {
 	{	
 		Debug.Log (other.gameObject.tag);
 		if (other.gameObject.tag == "Player") {
-			SceneManager.LoadScene("test2", LoadSceneMode.Single);
+			SceneManager.LoadScene(level, LoadSceneMode.Single);
 		}
 		}
 	}
