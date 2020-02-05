76d7c8302557c6b20285d31152d6e51e655e764d
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2020-02-05 13:33:00 +0300

    yufyf
    3 уровня

---
+++ MagnetGun/Assets/deadplane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class deadplane : MonoBehaviour
+{
+	public float x, y, z;
+	void OnTriggerEnter(Collider other)
+	{
+		other.transform.position = new Vector3(x, y, z);
+	}
+}
